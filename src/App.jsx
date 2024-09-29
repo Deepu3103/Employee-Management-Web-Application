53e40582fb709711328493ee0268470d72635018
Rayapura Deepu <55278143+Deepu3103@users.noreply.github.com>
2024-09-29 16:36:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-// import Linkdn from './components/Linkdn'
-// import { Articles } from './components/Articles'
-// import { Jobs } from './components/Jobs'
-// import { Games } from './components/Games'
-// import { People } from './components/People'
-// import { Learning } from './components/Learning'
 
 import Home from "./CRUD/Home"
 import CreateUser from './CRUD/CreateUser'
